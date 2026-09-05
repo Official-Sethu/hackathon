3d194b64a0921bf1c5d94fbb49c3d352bafaca7e
Trace Team <dev@trace.org>
2026-09-05 20:41:39 +0800

    fix: add missing kotlinx.coroutines.async import in GonkaRepository.kt

--- mobile-android/app/src/main/java/org/veritasgonka/app/GonkaRepository.kt
+++ mobile-android/app/src/main/java/org/veritasgonka/app/GonkaRepository.kt
@@ -1,6 +1,7 @@
 package org.veritasgonka.app
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import org.json.JSONObject
